b132c9e67d34a15d733870547c6aec0d43ce48c3
chuans <juaazn.05@gmail.com>
2024-09-03 17:00:08 +0200

    Reset

--- proyecto-frontend/src/main.jsx
+++ proyecto-frontend/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import Layout from './layout/Layout.jsx'
+import './layout.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Layout />
   </StrictMode>,
 )
