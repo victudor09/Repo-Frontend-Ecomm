fef013399ee0b4406368a95d52194efb38a58f25
chuans <juaazn.05@gmail.com>
2024-09-04 17:44:13 +0200

    context of product and login

--- proyecto-frontend/src/main.jsx
+++ proyecto-frontend/src/main.jsx
@@ -1,11 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { LoginContext } from './context/LoginContext.jsx'
+import { ProductContext } from './context/ProductContext.jsx'
 import Layout from './layout/Layout.jsx'
 import './layout.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Layout>
-    </Layout>
+    <LoginContext>
+      <ProductContext>
+        <Layout>
+        </Layout>
+      </ProductContext>
+    </LoginContext>
   </StrictMode>,
 )
