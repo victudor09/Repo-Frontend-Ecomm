907db4275bc71241a3673d0bf28cb0e6dd6b7c50
chuans <juaazn.05@gmail.com>
2024-09-04 17:44:22 +0200

    Component Categorie

---
+++ proyecto-frontend/src/components/Categorie.jsx
@@ -0,0 +1,12 @@
+import { useContext } from 'react'
+import { Product } from '../context/ProductContext'
+
+export default function Categorie () {
+  const { categorie } = useContext(Product)
+
+  return (
+    <>
+      <h4>{categorie.title}</h4>
+    </>
+  )
+}
