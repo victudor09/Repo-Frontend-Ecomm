b856548084bcb6436091eb93f887071c29636f53
chuans <juaazn.05@gmail.com>
2024-09-06 19:33:04 +0200

    main

--- proyecto-frontend/src/main.jsx
+++ proyecto-frontend/src/main.jsx
@@ -2,15 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { LoginContext } from './context/LoginContext.jsx'
 import { ProductContext } from './context/ProductContext.jsx'
-import Layout from './layout/Layout.jsx'
-import Home from './router/Home.jsx'
 import './layout.css'
 import App from './App.jsx'
-import Home from './components/Home.jsx'
-import Register from './components/Register.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <ProductContext>
+      <LoginContext>
+        <App />
+      </LoginContext>
+    </ProductContext>
+  </StrictMode>
 )
