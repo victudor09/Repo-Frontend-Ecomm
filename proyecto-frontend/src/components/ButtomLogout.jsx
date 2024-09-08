c46c271a90401f74d062a6939142c09dbe1b09a8
chuans <juaazn.05@gmail.com>
2024-09-09 01:11:48 +0200

    Navigate to home

--- proyecto-frontend/src/components/ButtomLogout.jsx
+++ proyecto-frontend/src/components/ButtomLogout.jsx
@@ -1,11 +1,14 @@
 import { useContext } from 'react'
 import { User } from '../context/UserContext'
+import { useNavigate } from 'react-router-dom'
 
 export default function ButtomLogout () {
+  const navigate =  useNavigate()
   const { logout } = useContext(User)
 
   const handleButtom = () => {
     logout()
+    navigate('/')
   }
 
   return (
