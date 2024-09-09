ea0d9807e14b3de13cc78ff341d05fce86ae720d
chuans <juaazn.05@gmail.com>
2024-09-09 18:19:37 +0200

    delete product

--- proyecto-frontend/src/components/Products.jsx
+++ proyecto-frontend/src/components/Products.jsx
@@ -2,14 +2,16 @@ import { useContext, useEffect } from 'react'
 import { Product } from '../context/ProductContext'
 
 const Products = () => {
-  const { products, getProduct } = useContext(Product)
+  const { products, getProduct, cartAdd } = useContext(Product)
 
   useEffect(() => {
     getProduct()
-}, [])
+  }, [])
 
-console.log('Productos en el componente:', products);
 
+  const handleCartAdd = (id) => {
+    cartAdd(id)
+  }
 
   return (
     <section className="Categories">
@@ -19,6 +21,7 @@ console.log('Productos en el componente:', products);
               <div key={product.id} className="category-item">
                 <h3>{product.name}</h3>
                 <p>${product.price}</p>
+                <button type="button" onClick={() => handleCartAdd(product.id)}>Add cart</button>
               </div>
           )) : <p>No hay producto</p>
         }
