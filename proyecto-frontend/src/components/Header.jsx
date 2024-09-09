90b3751e0a7102eed6fb792cd0377ff74a9e5d44
chuans <juaazn.05@gmail.com>
2024-09-09 02:47:38 +0200

    Conditional login and logout

--- proyecto-frontend/src/components/Header.jsx
+++ proyecto-frontend/src/components/Header.jsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import { User } from '../context/UserContext'
 import {Link} from 'react-router-dom'
 import ButtomLogout from './ButtomLogout'
@@ -6,28 +6,36 @@ import ButtomLogout from './ButtomLogout'
 import '../App.scss'
 
 function Header ()  {
-  const { user } = useContext(User)
-
+  const { user, token } = useContext(User)
+         
+  console.log('Estado de token',token)
   return (
     <nav className='Header'>
       <ul>
         <li>
           <Link to="/">Home</Link>
         </li>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-        <li>
-          <Link to="/register">Register</Link>
-        </li>
         {
-          user && (
-          <li>
-            <Link to="/profile">{user.name}</Link>
-          </li>)
+          token ? 
+          (
+          <>
+            <li>
+              <Link to="/profile">{user.name || 'Nombre de Usuario'}</Link>
+            </li>
+            <li>
+              <ButtomLogout />
+             </li>
+          </>) : 
+          (<>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/register">Register</Link>
+            </li>
+          </>)
         }
       </ul>
-      <ButtomLogout />
     </nav>
   )
 }
