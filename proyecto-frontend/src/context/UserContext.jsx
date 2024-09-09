90255e77ead57953819591bbd137861a5aad665e
chuans <juaazn.05@gmail.com>
2024-09-09 02:47:55 +0200

    Refactor console logs

--- proyecto-frontend/src/context/UserContext.jsx
+++ proyecto-frontend/src/context/UserContext.jsx
@@ -6,8 +6,8 @@ import { BASE_URL } from '../utils/constants.js'
 export const User = createContext()
 
 export function UserContext ({ children }) {
-  const token = JSON.parse(localStorage?.getItem('token'))
-  const initialStateUser = { user: null , token: token }
+  const token = JSON.parse(localStorage.getItem('token'))
+  const initialStateUser = { user: null , token: token ? token : null }
 
   const [ state, dispatch ] = useReducer(reducer, initialStateUser)
 
@@ -24,8 +24,7 @@ export function UserContext ({ children }) {
     try {
       const response = await axios.post(`${BASE_URL}/usuario/login`, loginUser)
       dispatch({ type: ACTIONS.LOGIN_USER, payload: response.data })
-      console.log('entra');
-      
+  
       if (response.data) return localStorage.setItem('token', JSON.stringify(response.data.token))
     } catch (error) {
       console.log(error)
@@ -34,11 +33,12 @@ export function UserContext ({ children }) {
 
   const logout = async () => {
     try {
-      console.log(token)
       const response = await axios.delete(`${BASE_URL}/usuario/logout`, {
         headers: { authorization: token }
       })
+
       dispatch({ type: ACTIONS.LOGOUT, payload: response.data })
+      
       if (response.data) return localStorage.removeItem('token')
     } catch (error) {
       console.log(error)
@@ -48,11 +48,7 @@ export function UserContext ({ children }) {
 /* PARA DESPACHAR DATOS DE USUARIO LOGEADO PRUEBO CON ESTO: */
   const getUser = async (getUser) => {
     try {
-      console.log(token);
-      
       const response = await axios.get(`${BASE_URL}/usuario/getAll`, {headers: {Authorization: `${token}`}}, getUser)
-      console.log(response.data);
-      
       dispatch({ type: ACTIONS.GET_USER, payload: response.data })
     } catch (error) {
       console.error(error)
