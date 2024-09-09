f2534f12a5ebab40f8e5fe93b77d0cfad657ecd4
victudor09 <victudor09@gmail.com>
2024-09-09 11:20:41 +0200

    ultimo merge

--- proyecto-frontend/src/components/Header.jsx
+++ proyecto-frontend/src/components/Header.jsx
@@ -15,6 +15,9 @@ function Header ()  {
         <li>
           <Link to="/">Home</Link>
         </li>
+        <li>
+          <Link to="/products">Products</Link>
+        </li>
         {
           token ? 
           (
